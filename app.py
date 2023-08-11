90a2f25c66111c29149992188ade1ee1fccd7f5c
BrayanStevenMorenoG <brasmoreno5@soy.sena.edu.co>
2023-08-11 09:00:53 -0500

    Realizado pa

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 #Iniciarlizar el objeto flask 
 app = Flask(__name__)
@@ -20,3 +21,23 @@ class Cliente(db.Model):
     username = db.Column(db.String(100), unique = True)
     password = db.Column(db.String(50))
     email = db.Column(db.String(100), unique = True)
+
+class Producto(db.Model):
+    __tablename__ = "productos"
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(120), unique = True)
+    precio = db.Column(db.Numeric(precision = 10, scale = 2))
+    imagen = db.Column(db.String(120), unique = True)
+
+class Ventas(db.Model):
+    __tablename__ = "ventas"
+    id = db.Column(db.Integer, primary_key = True)
+    fecha = db.Column(db.DateTime, default = datetime.utcnow)
+    cliente_id = db.Column(db.Integer, db.ForeignKey('clientes.id'))
+
+class Detalles(db.Model):
+    __tablename__ = "detalles"
+    id = db.Column(db.Integer, primary_key = True)
+    producto_id = db.Column(db.Integer, db.ForeignKey('productos.id'))
+    venta_id = db.Column(db.Integer, db.ForeignKey('ventas.id'))
+    cantidad = db.Column(db.Integer)
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config: 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/flask_shopy_2687386"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:1234@localhost/flask_shopy_2687386"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
