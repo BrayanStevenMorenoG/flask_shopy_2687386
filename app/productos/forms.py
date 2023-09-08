500394a8de1ebf71e73cd9856ae0f7c850c321d6
BrayanStevenMorenoG <brasmoreno5@soy.sena.edu.co>
2023-09-08 11:36:05 -0500

    CRUD COMPLETA

--- app/productos/forms.py
+++ app/productos/forms.py
@@ -3,20 +3,28 @@ from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import InputRequired, NumberRange
 from flask_wtf.file import *
 
-class NewProductForm(FlaskForm):
+class ProductForm():
     nombre = StringField("Ingrese el nombre del producto", 
-                         validators = [InputRequired( message = "llenalo chamo" )])
+                         validators = [InputRequired( message = "Llenar campo" )])
     precio = IntegerField("Ingrese el precio del producto",
-                          validators = [InputRequired( message = "Decime el precio"),
+                          validators = [InputRequired( message = "Llenar campo"),
                           NumberRange (min = 10000,
                                       max = 100000,
                                       message = "Bajale o Subile")])
+
+class NewProductForm(FlaskForm, ProductForm):
     Imagen = FileField(label = "Imagen",
                        validators = [
-                           FileRequired(message = "Ponga imagen marico"),
+                           FileRequired(message = "Ponga imagen"),
                             FileAllowed (
                                 ["jpg","png"], 
                                 message = "Formato invalido"
                             )
                        ])
     submit = SubmitField("Crear")
+
+class EditProductForm(FlaskForm, ProductForm):
+    submit = SubmitField("Actualizar")
+
+class DeleteProductForm():
+    submit = SubmitField("Eliminar")
